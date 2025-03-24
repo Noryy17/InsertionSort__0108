336f6568c5463452a507527399f815245d2925ce
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-03-24 12:38:59 +0700

    Membuat Kondisi n tidak lebih dari 20

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,22 @@ using namespace std;
 
 int arr[20] ; //Membuat Array Dengan Panjang data 20
 int n; // Membuat Variable Inoutan n
+
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <=20)
+        {//membuat kondisi n tidak lebih dari 20
+          break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Element.\n";
+        }
+    }
+    
+}
